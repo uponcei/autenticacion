0929d75729bee78948ee90bf8574c095d7dade4a
Uriel P. Ibarra <uriel.tar.gz@gmail.com>
2023-12-16 09:29:33 -0600

    Proyecto: Autenticacion
    Descripción: ajuste a la clase por problemas con las dependencias al momento de hacer el deploy

--- src/test/java/com/nmp/autenticacion/AutenticacionApplicationTests.java
+++ src/test/java/com/nmp/autenticacion/AutenticacionApplicationTests.java
@@ -1,13 +1,5 @@
 package com.nmp.autenticacion;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class AutenticacionApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
